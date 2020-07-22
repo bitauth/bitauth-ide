e7d6c2e29116c88a56dfd6c68696b960fe2a448f
Jason Dreyzehner <jason@dreyzehner.com>
2020-07-22 14:25:08 -0400

    test: add routing and example template e2e tests

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,16 +1,25 @@
 /// <reference types="cypress" />
 
-// TODO: cypress coverage https://github.com/cypress-io/instrument-cra
-// https://github.com/cypress-io/instrument-cra/issues/132
+// TODO: update project on resolution of: https://github.com/cypress-io/instrument-cra/issues/132
 
 describe('App', function () {
   beforeEach(function () {
     cy.visit('/');
   });
 
-  it('renders the app', function () {
+  it('renders the welcome pane', function () {
     cy.get('.bitauth').should('contain', 'bitauth');
     cy.get('.WelcomePane').should('contain', 'Choose a template to begin');
     cy.percySnapshot('WelcomePane');
+  });
+
+  it('loads the single signature template', function () {
+    cy.get('.WelcomePane').should('contain', 'Choose a template to begin');
+    cy.contains('Single Signature (P2PKH)').click();
+    cy.get('.EditorPane').should('contain', 'Single Signature (P2PKH)');
+    cy.contains('Unlock').click();
+    cy.get('.ScriptEditor > .title').contains('Unlock');
+    cy.get('.ScriptEditor > .editor').contains('schnorr_signature');
+    cy.percySnapshot('Single Signature (Unlock)');
   });
 });
